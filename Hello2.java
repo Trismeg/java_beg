3b5d33add64508d8e25559d8c4cd51a684c61769
Tony Schultz <tony.schultz@gmail.com>
2016-01-11 15:01:52 -0500

    added HelloWorld(s)

---
+++ Hello2.java
@@ -0,0 +1,10 @@
+public class Hello2 {
+
+    public static void main(String[] msds) {
+        System.out.print("Hello "+ msds[0]);
+        System.out.println(" "+ msds[1]);
+        System.out.println(msds);
+        
+    }
+
+}
---
+++ HelloWorld4.java
@@ -0,0 +1,8 @@
+public class HelloWorld4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello "+args[0]);
+        System.out.println("Hello "+args[1]);
+    }
+
+}
