592233201c81ca15e2436ff4df1c436a55005760
Tony Schultz <tony.schultz@gmail.com>
2016-01-12 13:38:20 -0500

    added Maths

---
+++ Maths.java
@@ -0,0 +1,11 @@
+public class Maths{
+  
+  public static void main(String[] args){
+   double base = 2.0;
+   double exponent = 0.5;
+   //This gets sqrt of two
+   double root= Math.sqrt(base);
+   System.out.println(root);
+   
+  }
+}
