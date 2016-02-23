01ff6a8fe5fa17edaafe627a7fcd03a4a629a752
Tony Schultz <tony.schultz@gmail.com>
2016-02-23 14:09:19 -0500

    EFstudent stuff

--- EFStudent.java
+++ EFStudent.java
@@ -1,13 +1,30 @@
 public class EFStudent{
   
   private String name;
-  public int id_num;
+  private int id_num;
   private String grade;
   
+  public EFStudent(){
+    this.name = new String("Jow Blow");
+    this.id_num = -1;
+    this.grade = new String("enrolled");
+  }
   
   public EFStudent( String a_name ){
    this.name=new String(a_name); 
    this.id_num = -1;
+   this.grade = new String("enrolled");
+  }
+  
+  public EFStudent( int an_id ){
+   this.name=new String("Jow Blow"); 
+   this.id_num = an_id;
+   this.grade = new String("enrolled");
+  }
+  
+  public EFStudent( String a_name, int an_id ){
+   this.name=new String( a_name ); 
+   this.id_num = an_id;
    this.grade = new String("enrolled");
   }
   
@@ -29,6 +46,10 @@ public class EFStudent{
   
   public String getGrade(){
     return this.grade;
+  }
+  
+  public void intro(EFStudent other){
+   System.out.println("Hello "+ other.getName() +other.getID()+ ".  My name is " + this.getName() +this.getID()); 
   }
 }
   
--- HelloEFStudent.java
+++ HelloEFStudent.java
@@ -8,7 +8,21 @@ public class HelloEFStudent{
     System.out.println(student1.getName() + " has the student ID number " + student1.getID());
     student1.setName( "Han To" );
     System.out.println(student1.getName() + " has the student ID number " + student1.getID());
-    System.out.println(student1.getName() + " has the student ID number " + student1.id_num);
+    
+    EFStudent student2 = new EFStudent( "Ohiz Ojo", 7);
+    student2.intro(student1);
+    
+    EFStudent[] cs101 = new EFStudent[21];
+    for(int i=0; i<cs101.length ; i++){
+      cs101[i] = new EFStudent(1000+i);
+    }
+    
+    System.out.println("the 10th sudent is " + cs101[9].getName() + " with id# "+ cs101[9].getID());
+    
+    for(int i=0; i<cs101.length; i++){
+      cs101[i].intro(cs101[(i+1)%21]);
+      cs101[(i+1)%21].intro(cs101[i]);}
+    
   }
   
 }
