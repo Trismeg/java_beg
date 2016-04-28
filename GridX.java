2bf77f9d7f89dfde54565c5cfe5a22393b292ed2
Tony Schultz <tony.schultz@gmail.com>
2016-04-28 13:12:17 -0400

    halp

--- GridX.java
+++ GridX.java
@@ -1,9 +1,7 @@
 public class GridX{
   
-  static int D = 10;
+  static int D = 5;
   static int[][] grid = new int[D][D];
-  static int x=3;
-  static int y=5;
   static int[][] gridB = new int[D][D];
   static boolean[][] gridC = new boolean[D][D];
   static int total=0;
@@ -15,16 +13,8 @@ public class GridX{
         else {grid[i][j]=0;}
       }}}
   
-  public static void connect(int a, int b){
-    if((a<0 | a>(D-1))|(b<0 | b>(D-1))){}
-    else if(gridC[a][b]){}
-    else if(grid[a][b]==0){gridC[a][b]=true;}
-    else {
-      total=total+1;
-      gridB[a][b]=1;
-      connect(a-1,b); connect(a+1,b); connect(a,b-1); connect(a,b+1);
-    }}
-      
+  
+       
   public static void print( int[][] arr){
     for(int i=0; i<arr.length; i++){
       for(int j=0; j<arr.length; j++){
@@ -34,12 +24,13 @@ public class GridX{
     
   
   public static void main(String[] args){
-    fill(0.33);
+    fill(0.5);
     print(grid);
-    print(gridB);
-    connect(x,y);
-    print(gridB);
-    
+    //print(gridB);
+    //connect(1,1);
+    System.out.println("-----------------");
+    //print(gridB);
+   
   
 }
 }
