54d4ef427cbf751446c92309a4ff52edb714764a
Tony Schultz <tony.schultz@gmail.com>
2016-05-10 14:24:20 -0400

    TNode methods

--- TNode.java
+++ TNode.java
@@ -24,23 +24,52 @@ public TNode getI(int i){
       if(this.getI(i)==null){return i;}
       i++;}
   }
+ 
+ public void add(TNode newnode){
+   int open = this.empty();
+   int parent=open/2;
+   if((open%2)==0){
+     this.getI(parent).left=newnode;}
+   else{this.getI(parent).right=newnode;}}
 
+ public int count(){
+  int sum = 0;
+  if(this==null){}
+  else{sum=sum+1;}
+  if(this.left==null){}
+  else{sum=sum+this.left.count();}
+  if(this.right==null){}
+  else{sum=sum+this.right.count();}
+  return sum;
+ }
+ 
  public static void main(String[] args){
     TNode node1 = new TNode();
     TNode node2 = new TNode();
     TNode node3 = new TNode();
     TNode node4 = new TNode();
     TNode node5 = new TNode();
+    TNode nodeB = new TNode();
     node1.left=node2;
     node1.right=node3;   
     node2.left=node4;
     node2.right=node5;
+    node3.right=nodeB;
        
-    for(int i=1;i<7;i++){
+    for(int i=1;i<10;i++){
     System.out.println(i+" " + node1.getI(i));
     }
     
     System.out.println("empty " + node1.empty());    
-  }
+  System.out.println("count " + node1.count());  
  
+ TNode nodeA = new TNode();
+ node1.add(nodeA);
+ 
+ for(int i=1;i<10;i++){
+    System.out.println(i+" " + node1.getI(i));
+    }
+ System.out.println("empty " + node1.empty());
+ System.out.println("count " + node1.count()); 
+ } 
 }
